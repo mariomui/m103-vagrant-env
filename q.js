64b63a7c6c20640da8fbbdb572248196df6783f8
mariomui <mmui@riffyn.com>
2019-05-21 09:04:14 -0700

    --amend

--- q.js
+++ q.js
@@ -3,3 +3,9 @@ db.createUser({
   pwd: "m103-application-pass",
   roles: [{role: "readWrite", db: "applicationData"}]
 })
+
+db.createUser({
+  user: "m103-admin",
+  pwd: "m103-pass",
+  roles: [{role: "root", db: "admin"}]
+})
